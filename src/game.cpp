de4d52f09786b032e2cbe474655087f726ac4874
Enator18 <62544405+Enator18@users.noreply.github.com>
2026-01-28 17:15:26 -0500

    set up empty game

--- src/game.cpp
+++ src/game.cpp
@@ -2,23 +2,8 @@
 
 #include <game.h>
 #include <engine.h>
-#include <city_builder.h>
 #include <scene.h>
 
-#if MARVIN_GAME
-#include <marvin_systems.h>
-#endif
+void OnGameStart(GameState* gameState) {}
 
-#include <movement.h>
-
-void OnGameStart(GameState* gameState, std::string mapName)
-{
-    Scene &scene = gameState->scene;
-    b32 slowStep = false;
-
-#if MARVIN_GAME
-    scene.CreateVariableTimestepSystem<GravityBallsSystem>();
-#endif
-    scene.CreateSemifixedTimestepSystem<MovementSystem>();
-    scene.CreateSemifixedTimestepSystem<BuilderSystem>(slowStep);
-}
+void OnEditorStart(GameState* gameState) {}
